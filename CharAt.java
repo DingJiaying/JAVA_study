7740f93e70a6b49d43cb93eacd57964d84f40a72
Ding Jiaying <18305186709@163.com>
2017-09-06 22:02:53 +0800

    Change3 in 0906 for String

---
+++ CharAt.java
@@ -0,0 +1,13 @@
+package demo3;
+/**
+ * 求出字符串中指定位置的字符
+ * @author ding
+ *
+ */
+public class CharAt {
+	public static void main(String[] args) {
+		String str = "helloJava!!!" ;
+        System.out.println(str.charAt(0)) ;//返回索引处的char值
+        System.out.println(str.charAt(3)) ;
+	}
+}
