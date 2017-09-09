d9887d998b8d9471bbd8301cac0a8a7944771b72
Ding Jiaying <18305186709@163.com>
2017-09-09 22:08:54 +0800

    Change14 in 0909 for leiji

---
+++ CollectionsDemo.java
@@ -0,0 +1,32 @@
+package demo15;
+/**
+ * 练习Collections的使用
+ * @author ding
+ *
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionsDemo {
+	public static void main(String[] args) {
+		double array[] = {110.12,28.55,56.89,345};     //定义数组并赋值
+		ArrayList<Double>  list = new ArrayList<Double>();//实例化集合对象
+		
+		for (int i = 0; i < array.length; i++) 
+		{
+			list.add(new Double(array[i]));
+		}
+		Collections.sort(list);
+		System.out.println("----数组排序后-----");	
+		for (int i = 0; i < array.length; i++) 
+		{
+		  System.out.println(list.get(i));	
+	    }
+		System.out.println("----数组反转后-----");	
+		Collections.reverse(list);
+		for (int i = 0; i < array.length; i++) 
+		{
+		  System.out.println(list.get(i));	
+	    }
+	}
+}
