9add632b41ff87c6599fb0972dca6f272a867bb4
Ding Jiaying <18305186709@163.com>
2017-09-24 22:28:21 +0800

    Change6 in 0924

---
+++ SystemPrintDemo.java
@@ -0,0 +1,20 @@
+package demo6;
+/**
+ * 练习打印流的使用
+ * @author ding
+ *
+ */
+
+import java.io.* ;
+
+public class SystemPrintDemo {
+	public static void main(String args[])
+    {
+        PrintWriter out = null ;
+        // 通过System.out对PrintWriter实例化
+        out = new PrintWriter(System.out) ;
+        // 向屏幕上输出
+        out.print("Hello World!") ;
+        out.close() ;
+    }
+}
